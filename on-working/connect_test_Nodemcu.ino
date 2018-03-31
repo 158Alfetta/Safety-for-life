58717f072a692444478fb319b2aa6e6013134714
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2018-03-31 19:06:11 +0700

    testing on function.

---
+++ on-working/connect_test_Nodemcu.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+
+#include <SoftwareSerial.h>
+
+char income = 'x';
+
+SoftwareSerial NodeSerial(D2,D3); // RX | TX
+
+void setup() {
+
+  pinMode(D2, INPUT); 
+
+  pinMode(D3, OUTPUT); 
+
+  Serial.begin(9600);
+
+  NodeSerial.begin(57600);
+
+  Serial.println();
+
+  Serial.println();
+
+
+Serial.println("NodeMCU/ESP8266 Run");
+
+ }
+void loop() {
+
+  while (NodeSerial.available() > 0) 
+
+  {
+
+    int i_data = NodeSerial.parseInt(); 
+
+    float f_data = NodeSerial.parseFloat();
+    //income = NodeSerial.read();
+
+    if (NodeSerial.read() == '\n') 
+
+    {
+
+      Serial.println(income);
+
+          Serial.print("NodeMCU or ESP8266"); Serial.print(" : "); 
+          Serial.print(i_data); Serial.print(" : "); 
+
+         Serial.println(f_data);  
+
+    }
+
+    delay(50);
+
+  }
+
+  delay(10);
+
+} 
